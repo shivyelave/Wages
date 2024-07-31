f6408febce780ec6e3f9a408e5df98c3b750ee21
shivyelave <syelave20ite@student.mes.ac.in>
2024-07-31 15:36:57 +0530

    [Shiv] Add: Employee daily wage.

--- emp.py
+++ emp.py
@@ -34,11 +34,35 @@ def employee_attendance():
         return 0  # Absent
     if attendance == 1:
         return 1  # Present
+    
+
+def calculate_wage_of_employee():
+    
+    """
+
+    Description:
+        Function to calculate the wage of an employee based on their attendance.
+
+    Returns:
+        int: The wage of the employee based on their attendance:
+            - 0 for no attendance
+            - 20*8 for full day attendance
+
+    
+    """
+    
+    attendance = employee_attendance()  # Get attendance status from employee_attendance function
+
+    if attendance == 0:
+        return 0  # No attendance, no wage
+
+    if attendance == 1:
+        return 20 * 8  # Full-day attendance, wage for 8 hours at $20/hour
 
 
 def main():
     # Call the employee_attendance function and print the result
-    print(employee_attendance())
+    print(calculate_wage_of_employee())
 
 # If this script is run as the main module, execute the main function
 if __name__ == '__main__':
